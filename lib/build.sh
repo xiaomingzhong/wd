46da7ce2d939294430fdff99b6556cec026c582b
wangxun <soa2011@qq.com>
2015-11-19 19:59:37 +0800

    m

--- lib/build.sh
+++ lib/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 mkdir classes
-javac -encoding gbk -d classes -classpath tdbapi.jar ../Demo/tdbapi/Wddl.java
+javac -encoding utf-8 -d classes -classpath tdbapi.jar ../Demo/tdbapi/Wddl.java
 cd classes
 jar -cvf ../wddl.jar ./ 
 
